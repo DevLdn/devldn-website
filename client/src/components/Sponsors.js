df4febe41553687f7c58b399eed2255adb7c1acd
Caryn <c.humphreys@vehikl.com>
2019-05-03 16:12:21 -0400

    adds fonts back in and creates new sponsors feature

--- client/src/components/Sponsors.js
+++ client/src/components/Sponsors.js
@@ -5,62 +5,69 @@ import infotech from '../assets/infotech.svg';
 class Sponsors extends Component {
   render() {
     return (
-      <div className="sponsor">
-        <div className="sponsors-container">
-          <div className="current">
-            <h1>Current Sponsors</h1>
+      <div className="shoutout">
+        <div className="shoutout-container">
+          <div className="mission">
+            <h2 className="small">Our Mission</h2>
             <p>
-              This group is made possible by the generosity of our monthly
-              sponsors.
+              Dev London is your opportunity to gain insights and inspiration by
+              networking with influential software professionals.
             </p>
-            <div className="sponsors">
-              <a
-                href="https://www.infotech.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="logo grow shadow">
+          </div>
+          <div className="sponsors">
+            <div className="current">
+              <h1>Current Sponsors</h1>
+              <p>
+                This group is made possible by the generosity of our monthly
+                sponsors.
+              </p>
+              <div className="sponsors-gallery">
+                <a
+                  href="https://www.infotech.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="partner-logo grow shadow"
+                >
                   <img
                     src={infotech}
                     className="full-width"
                     alt="Info Tech Research Group logo"
                   />
-                </div>
-              </a>
-              <a
-                href="https://wesbos.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="logo grow shadow wes">
+                </a>
+                <a
+                  href="https://wesbos.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="partner-logo grow shadow wes"
+                >
                   <img src={wes} alt="Wes Bos logo" />
-                </div>
-              </a>
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="past">
-            <h2>Past Sponsors</h2>
-            <div className="sponsors-list">
-              <div className="sponsor-list-item">
-                <a
-                  href="http://ztr.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  ZTR
-                </a>{' '}
-                (venue sponsor)
-              </div>
-              <div className="sponsor-list-item">
-                <a
-                  href="https://vehikl.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Vehikl
-                </a>{' '}
-                (venue sponsor, Meetup sponsor)
-              </div>
+            <div className="past">
+              <h2>Past Sponsors</h2>
+              <ul className="sponsors-list">
+                <li className="sponsor-list-item">
+                  <a
+                    href="http://ztr.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    ZTR
+                  </a>{' '}
+                  (venue sponsor)
+                </li>
+                <li className="sponsor-list-item">
+                  <a
+                    href="https://vehikl.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Vehikl
+                  </a>{' '}
+                  (venue sponsor, Meetup sponsor)
+                </li>
+              </ul>
             </div>
           </div>
         </div>
