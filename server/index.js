9cc4768d93cc00f3eac5e422dfe4417bd58da8e3
Jesse O'Brien <jesse@jesse-obrien.ca>
2019-06-19 15:57:16 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const fetch = require('node-fetch');
 const validator = require('email-validator');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 const HOST = process.env.HOST || 'localhost';
 
 app.use(bodyParser.json());
